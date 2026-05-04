d76ee736cc4011336b2b25fb0d2a95d49efc2f65
NicoSciangula <nico.sciangula99@gmail.com>
2026-05-04 16:20:32 +0200

    imported router in app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// * Importato il router
+const movieRouter = require("./router/movieRouter");
+
+app.use(express.json());
+
+app.use("/api/movies", movieRouter);
+
 app.get("/", (req, res) => {
   res.send("Benvenuto sul mio sito!");
 });
