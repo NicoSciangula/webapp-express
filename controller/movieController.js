f6a52692660be2fd6389cc4508628edb8572cc43
NicoSciangula <nico.sciangula99@gmail.com>
2026-05-04 16:58:47 +0200

    added query index

--- controller/movieController.js
+++ controller/movieController.js
@@ -1,7 +1,12 @@
 const connection = require("./../data/db");
 
 function index(req, res) {
-  res.send("lista film");
+  const sql = "SELECT * FROM movies ";
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({error: "Database query failed"});
+    res.json(results);
+  });
 }
 
 function show(req, res) {
