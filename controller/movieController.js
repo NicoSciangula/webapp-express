6e9ee66340f7d66fbdb8f4e48a1495cd7f8dc89f
NicoSciangula <nico.sciangula99@gmail.com>
2026-05-04 16:04:17 +0200

    added movieController with function index and show

---
+++ controller/movieController.js
@@ -0,0 +1,9 @@
+function index(req, res) {
+  res.send("lista film");
+}
+
+function show(req, res) {
+  res.send(`Il film che hai selezionato è il ${req.params.id}`);
+}
+
+module.exports = {index, show};
